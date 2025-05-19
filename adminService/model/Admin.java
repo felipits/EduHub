c75d926f8978b83bc62837f117aafcb12e14128a
Felipe Villalobos <fel.villalobos@duocuc.cl>
2025-05-19 19:23:21 -0400

    Add files via upload

---
+++ adminService/model/Admin.java
@@ -0,0 +1,32 @@
+package com.EduHubAcademy.adminService.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "service_admin")
+
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private  String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasenia;
+
+
+
+
+
+
+}
