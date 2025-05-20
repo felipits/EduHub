2596a71f01ab056883c1b25427eca1ae4a64b02f
EmilioJaramillo <168316621+EmilioJaramillo@users.noreply.github.com>
2025-05-19 20:14:02 -0400

    Delete EduHub-20250505T232430Z-1-001/EduHub-20250505T232430Z-1-001/EduHub directory

--- EduHub-20250505T232430Z-1-001/EduHub-20250505T232430Z-1-001/EduHub/src/main/java/com/EduHubAcademy/asignaturaService/repository/AsignaturaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.EduHubAcademy.asignaturaService.repository;
-
-import com.EduHubAcademy.asignaturaService.model.Asignatura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AsignaturaRepository extends JpaRepository<Asignatura, Long> {
-}
--- EduHub-20250505T232430Z-1-001/EduHub-20250505T232430Z-1-001/EduHub/src/main/java/com/EduHubAcademy/soporteService/repository/SoporteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.EduHubAcademy.soporteService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.EduHubAcademy.soporteService.model.Soporte;
-
-public interface SoporteRepository extends JpaRepository<Soporte, Long>{
-
-}
